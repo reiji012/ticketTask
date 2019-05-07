cfb3798d3a55e1c70b2ab7f5f3555dbc43835217
reiji <reiji0120124@gmail.com>
2019-05-07 13:48:11 +0900

    first commit

---
+++ TicketTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicketTask
+//
+//  Created by 松村礼二 on 2019/05/07.
+//  Copyright © 2019 松村礼二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
