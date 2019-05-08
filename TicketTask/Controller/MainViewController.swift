d2b7db3ca94b889c1ad521be25794104dad27c95
reiji <reiji0120124@gmail.com>
2019-05-08 11:39:41 +0900

    TaskViewコード修正

--- TicketTask/Controller/MainViewController.swift
+++ TicketTask/Controller/MainViewController.swift
@@ -24,23 +24,14 @@ class MainViewController: UIViewController,UIScrollViewDelegate {
     }
     
     func bindUI() {
-        //グラデーションの開始色
+        
+        //グラデーションの作成
         let topColor = UIColor(red:0.57, green:0.63, blue:0.96, alpha:1)
-        //グラデーションの開始色
         let bottomColor = UIColor(red:0.54, green:0.74, blue:0.74, alpha:1)
-        
-        //グラデーションの色を配列で管理
         let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
-        
-        //グラデーションレイヤーを作成
         let gradientLayer: CAGradientLayer = CAGradientLayer()
-        
-        //グラデーションの色をレイヤーに割り当てる
         gradientLayer.colors = gradientColors
-        //グラデーションレイヤーをスクリーンサイズにする
         gradientLayer.frame = self.view.bounds
-        
-        //グラデーションレイヤーをビューの一番下に配置
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
