5485e632cd953e6dcde2b7f26a32529db59952dc
Reiji <reiji0120124@gmail.com>
2019-05-18 18:35:19 +0900

    Rxversion変更

---
+++ TicketTask/TaskItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskItem.swift
+//  TicketTask
+//
+//  Created by 松村礼二 on 2019/05/15.
+//  Copyright © 2019 松村礼二. All rights reserved.
+//
+
+import UIKit
+
+class TaskItem: NSObject {
+
+}
